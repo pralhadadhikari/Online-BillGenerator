30aaf560e5130d69f625b57c7612991ecf5bc1ab
pralhadadhikari <101581753+pralhadadhikari@users.noreply.github.com>
2023-01-04 00:02:34 +0545

    payment bill modal added
    payment bill modal added

--- BillGenerator/wwwroot/js/listofitem.js
+++ BillGenerator/wwwroot/js/listofitem.js
@@ -34,131 +34,128 @@
 //        }
 //    });
 
-var dtable;
+//var dtable;
 
-$(document).ready(function () {
-
-
-    $("input").change(function () {
-        total();
-    });
-
-    $("#ItemId").change(function () {
-
-        itemPrice();
-    });
-
-    $("#AddToList").click(function () {
-        addItemToList();
-    });
-
-    $("#AddToList").click(function () {
-        GrandTotal();
-    });
-    //var itemId = $("#Item").val();
+//$(document).ready(function () {
 
 
-    function itemPrice() {
+//    $("input").change(function () {
+//        total();
+//    });
+
+//    $("#ItemId").change(function () {
+
+//        itemPrice();
+//    });
+
+//    $("#AddToList").click(function () {
+//        addItemToList();
+//    });
+
+//    $("#AddToList").click(function () {
+//        GrandTotal();
+//    });
+//    //var itemId = $("#Item").val();
 
 
-        $(function () {
+//    function itemPrice() {
 
-            $.ajax({
 
-                url: "/Home/getUnitPrice",
-                datatype: "Json",
-                data: { Id: $('#ItemId').val() },
+//        $(function () {
 
-                success: function (data) {
-                    $('#Price').val(data);
+//            $.ajax({
+
+//                url: "/Home/getUnitPrice",
+//                datatype: "Json",
+//                data: { Id: $('#ItemId').val() },
+
+//                success: function (data) {
+//                    $('#Price').val(data);
                     
-                }
-            });
-        });
-    }
+//                }
+//            });
+//        });
+//    }
 
 
 
-    function total() {
-        var UnitPrice = $("#Price").val();
-        var Quantity = $("#Quantity").val();
-        var Discount = $("#Discount").val();
-        var amount = parseInt(UnitPrice) * parseInt(Quantity);
-        Total = (amount - (amount * (parseInt(Discount)) * .01));
-        $("#Total").val(Total);
-    };
+//    function total() {
+//        var UnitPrice = $("#Price").val();
+//        var Quantity = $("#Quantity").val();
+//        var Discount = $("#Discount").val();
+//        var amount = parseInt(UnitPrice) * parseInt(Quantity);
+//        Total = (amount - (amount * (parseInt(Discount)) * .01));
+//        $("#Total").val(Total);
+//    };
 
-    function addItemToList() {
+//    function addItemToList() {
 
-        var tblItem = $("#myTable");
+//        var tblItem = $("#myTable");
 
-        var UnitPrice = $("#Price").val();
-        var Quantity = $("#Quantity").val();
-        var Discount = $("#Discount").val();
+//        var UnitPrice = $("#Price").val();
+//        var Quantity = $("#Quantity").val();
+//        var Discount = $("#Discount").val();
 
-        //var Snum = $("#myTable tbody").children().length + 1;
-        var ItemName = $("#ItemId option:selected").text();
-        var ItemIdd = $("#ItemId option:selected").val();
-        var Amount = UnitPrice * Quantity;
-        var Total = (Amount - (Amount * Discount) * .01)
+//        //var Snum = $("#myTable tbody").children().length + 1;
+//        var ItemName = $("#ItemId option:selected").text();
+//        var ItemIdd = $("#ItemId option:selected").val();
+//        var Amount = UnitPrice * Quantity;
+//        var Total = (Amount - (Amount * Discount) * .01)
 
-        var ItemList = "<tr><td>"
-            + ItemIdd +
-            "</td><td>"
-            + ItemName +
-            "</td><td>"
-            + UnitPrice +
-            "</td><td>"
-            + Quantity +
-            "</td><td>"
-            + Discount +
-            "</td><td>"
-            + Total +
-            "</td><td><button type='submit' class='btn btn-danger' id= 'RemoveItem'>Remove</button></td></tr>";
+//        var ItemList = "<tr><td>"
+//            + ItemIdd +
+//            "</td><td>"
+//            + ItemName +
+//            "</td><td>"
+//            + UnitPrice +
+//            "</td><td>"
+//            + Quantity +
+//            "</td><td>"
+//            + Discount +
+//            "</td><td>"
+//            + Total +
+//            "</td><td><button type='submit' class='btn btn-danger' id= 'RemoveItem'>Remove</button></td></tr>";
 
-        tblItem.append(ItemList);
-        calculateGT();
-        ResetItem();
+//        tblItem.append(ItemList);
+//        calculateGT();
+//        ResetItem();
 
-        $("#RemoveItem").click(function () {
-            $(this).parent().parent().remove();
+//        $("#RemoveItem").click(function () {
+//            $(this).parent().parent().remove();
 
-        });
+//        });
 
-        
-
-
-    }
+//    }
 
 
 
-    function calculateGT() {
-        $("#GrandTotal").val("0.00");
-        var FinalTotal = 0.00;
+//    function calculateGT() {
+//        $("#GrandTotal").val("0.00");
+//        var FinalTotal = 0.00;
 
-        $("#myTable").find("tr:gt(0)").each(function(){
-            var Total = parseFloat($(this).find("td:eq(5)").text());
-            FinalTotal += Total;
+//        $("#myTable").find("tr:gt(0)").each(function(){
+//            var Total = parseFloat($(this).find("td:eq(5)").text());
+//            FinalTotal += Total;
            
-        });
+//        });
 
-        $("#GrandTotal").val(FinalTotal); 
+//        $("#GrandTotal").val(FinalTotal); 
 
-    }
+//    }
 
      
-    function ResetItem() {
-        $("#Price").val('');
-        $("#Quantity").val('');
-        $("#Discount").val('0');
-        $("#ItemId").val('');;
-        $("#Total").val('');
+//    function ResetItem() {
+//        $("#Price").val('');
+//        $("#Quantity").val('');
+//        $("#Discount").val('0');
+//        $("#ItemId").val('');;
+//        $("#Total").val('');
 
-    }
+//    }
 
        
 
-});
+//});
 
 
 
